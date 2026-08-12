1116cf5d5901ae8e09c6756cb7475068fa84edac
Punit2908 <punitjangra2742@gmail.com>
2026-08-12 16:33:24 +0530

    variables update

--- JavaScript/variables.js
+++ JavaScript/variables.js
@@ -18,7 +18,7 @@ let b //declaration- maine variable to bana diya but use koi value nhi di
 //Undefined result dega
 b = 88 // initialization- variable ko value dena(string, number, symbol)
 const c = 10//constant me undefined nhi ja skta aur use sirf declare krke nhi chod skte
-
+// c = 20
 //hamari site pe jo bhi dikha hai vo ek window ka part hai
 
 var number//declare
@@ -35,3 +35,24 @@ console.log(num,num2,name,name2);
 
 console.log("This is an example line")
 
+let aaa
+aaa = 10
+console.log(aaa);
+
+//typesOf variables number,string,symbol,boolean,undefined,null,NaN,infinity
+
+let infy = 1/0
+console.log(infy);
+let no = null
+console.log(no);
+// let stake = 100>90
+let stake = true
+console.log(stake);
+let frac = 2/5
+console.log(frac);
+
+
+console.log(typeof(num))
+console.log(typeof(no))//js has error in print this type and shows object instead of null
+
+// let nan = parseInt(prompt("Enter a number"))
