7cd5e2f7e773c790dd0075cb841449351cb83247
Punit2908 <punitjangra2742@gmail.com>
2026-08-17 16:21:52 +0530

    local and global scope with strings introduction

---
+++ JavaScript/ScopeVariable.js
@@ -0,0 +1,18 @@
+let a = 10;//global
+{
+    let a = 11//local
+    console.log(a);
+}
+console.log(a);
+// confirm("Do you want to continue")
+// alert("Do you want to continue")
+// let num = parseInt(prompt("Enter a number"))
+// parseInt(num)
+
+//string
+const str = "Hello World"
+
+console.log(str.length);
+console.log(str[5]);
+console.log(str);
+
