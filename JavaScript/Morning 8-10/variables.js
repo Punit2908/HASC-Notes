ab477c413acb6e0648ec8cd5b3077e587c5e009f
Punit2908 <punitjangra2742@gmail.com>
2026-08-18 09:34:58 +0530

    Morning css

---
+++ JavaScript/Morning 8-10/variables.js
@@ -0,0 +1,47 @@
+//variables
+let num = 20
+console.log(num);
+// console.log(21);
+// console.log(num*2);
+// console.log(num**2);
+// console.log(num/2);
+// console.log(5%2); remainder
+
+
+
+//string
+let name = "Munish"
+console.log("Hello, my name is"+name);
+
+//bool
+// let bool = true
+let bool = 20<10
+console.log(bool);
+
+//null,NaN
+// let a = null js null ke datatype ko object batata hai jbki null apne aap me ek datatype hai
+let a = 0/0
+console.log(a);
+console.log(typeof(a));
+
+
+//undefined
+let b;  //declaration
+console.log(b);
+b = 10; //initialization
+
+
+let name1 = "Munish"
+let name2 = "Kumar"
+console.log(name1+" "+name2);
+
+//infinety
+let infy = 1/0
+console.log(infy);
+
+//urinary operators
+// num-=5
+// num+=5
+// num*=5
+num/=5
+console.log(num);
