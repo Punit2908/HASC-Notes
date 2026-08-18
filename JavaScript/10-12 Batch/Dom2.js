54b2bf410b9bc62fda89e0a848f764146e864881
Punit2908 <punitjangra2742@gmail.com>
2026-08-18 10:52:24 +0530

    Dom class 2 10-12

---
+++ JavaScript/10-12 Batch/Dom2.js
@@ -0,0 +1,26 @@
+// let div = document.querySelector("div")
+
+// div.innerText = "This text is changed with js"
+
+// document.write = "This is added from document.write"
+
+let usr = document.getElementById("username")
+let lst = document.getElementById("lastname")
+// let btn = document.querySelector("button")
+// function fullname(){
+//     console.log(usr.value+" "+lst.value);
+// }
+
+// btn.addEventListener("click",(e)=>{
+//     e.preventDefault()
+//     fullname()
+// })
+
+// console.log(usr,lst,btn);
+let p = document.getElementsByClassName("output").item(0)
+
+usr.addEventListener("input",(event)=>{
+    event.preventDefault()
+    // console.log(event);
+    p.innerText = usr.value
+})
