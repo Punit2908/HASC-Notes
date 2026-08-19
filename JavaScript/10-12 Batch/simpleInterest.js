6ba2cd067e754c0a5261d6ed65234a830de9c5d5
Punit2908 <punitjangra2742@gmail.com>
2026-08-19 10:45:01 +0530

    8-10 batch css class 1

--- JavaScript/10-12 Batch/simpleInterest.js
+++ JavaScript/10-12 Batch/simpleInterest.js
@@ -1,13 +1,16 @@
 
 
 function calculateAmount() {
-    let principle = document.getElementById("principle").value;
+  let principle = document.getElementById("principle").value;
   let time = document.getElementById("time").value;
   let rate = document.getElementById("rate").value;
   let amount = principle * (1 + rate/100) * time;
   console.log(principle,time,rate)
-  document.getElementById("amount").value = amount;
-
+  // document.getElementById("amount").value = amount;
+  let p = document.getElementsByClassName("output").item(0)
+  p.innerText = amount
+  console.log(p.innerText)
+  
 }
 let btn = document.querySelector(".submit")
 
@@ -23,4 +26,17 @@ let btn2 = document.querySelector(".change-css")
 btn2.addEventListener("click",()=>{
   box.innerText = "The text is changed";
   box.classList.add("box")
+})
+
+document.addEventListener("keydown",(e)=>{
+  e.preventDefault()
+  if(e.key == "Enter"){
+    console.log("Enter pressed");
+  }
+  
+})
+
+document.getElementsByClassName("experiment").item(0).addEventListener("dblclick",(e)=>{
+  console.log("double clicked");
+  
 })
