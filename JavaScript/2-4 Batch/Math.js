ee3c45ca2213bd9bd29c70e11fda5e6ee6d4fb16
Punit2908 <punitjangra2742@gmail.com>
2026-08-25 08:56:31 +0530

    Math.js update

---
+++ JavaScript/2-4 Batch/Math.js
@@ -0,0 +1,20 @@
+console.log(Math);
+console.log(Math.E);
+Math.PI = 3.14
+console.log(Math.PI);
+
+
+let num = 16
+// console.log(Math.sqrt(num));
+console.log(Math.pow(16,3));
+
+console.log(Math.floor(-2.99))
+console.log(Math.ceil(-2.1))
+console.log(Math.round(-2.5))
+
+console.log(Math.random()); //0<x<1
+console.log(Math.round(Math.random()*10)+10); 
+
+console.log(5&&-1);
+
+//if-else
---
+++ JavaScript/Morning 8-10/Math.js
@@ -0,0 +1,56 @@
+// Math in js
+
+console.log(Math.PI);
+Math.PI = 3.14
+console.log(Math.PI);
+
+console.log(Math.pow(16,16))
+console.log(Math.sqrt(164))
+let num = 12.5
+let num2 = -12.1
+console.log(Math.ceil(num2))
+console.log(Math.floor(num2))
+console.log(Math.round(num2))
+
+
+// console.log(Math.random()*10);
+console.log(Math.floor(Math.random()*10));
+console.log(Math.ceil(Math.random()*10));
+console.log(Math.round(Math.random()*10));
+console.log(Math.floor(Math.random()*10)+10);
+console.log(Math.ceil(Math.random()*10)+10);
+console.log(Math.round(Math.random()*10)+10);
+
+
+//if else
+let age = parseInt(prompt("Enter your age"))
+//18>18
+if(age>=18){
+    console.log("You are eligible to vote");
+}else{
+    console.log("You are not eligible to vote");
+}
+console.log("Hello Munish");
+
+
+//additional info
+console.log(Math);
+console.log(Math.E);
+Math.PI = 3.14
+console.log(Math.PI);
+
+
+let num = 16
+// console.log(Math.sqrt(num));
+console.log(Math.pow(16,3));
+
+console.log(Math.floor(-2.99))
+console.log(Math.ceil(-2.1))
+console.log(Math.round(-2.5))
+
+console.log(Math.random()); //0<x<1
+console.log(Math.round(Math.random()*10)+10); 
+
+console.log(5&&-1);
+
+//if-else
